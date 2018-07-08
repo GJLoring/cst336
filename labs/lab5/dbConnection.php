cbf6f95f7e5ab2d2ab57b537711d821a7312ff5b
Gabriel Loring <gloring@csumb.edu>
2018-07-08 01:29:37 +0000

    lab5

--- labs/lab5/dbConnection.php
+++ labs/lab5/dbConnection.php
@@ -8,6 +8,20 @@
         $host = "localhost";
         $username = "root";
         $password = "";
+
+        //when connecting from Heroku
+        if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
+            $host = "localhost";
+            $username = "b6ef724ec50a2b";
+            $password = "8db34bf9";
+        
+            $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+            $host = $url["host"];
+            $dbname = substr($url["path"], 1);
+            $username = $url["user"];
+            $password = $url["pass"];
+        } 
+
         $dbConn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
         $dbConn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
         return $dbConn;
